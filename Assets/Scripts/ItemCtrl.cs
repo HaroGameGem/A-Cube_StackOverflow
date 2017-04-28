127006b3b4eb04ca6483fb51d7923a97e1df2eda
YuGene <bbiggoagi@gmail.com>
2017-04-29 04:43:50 +0900

    아이템 컨트롤 수정

--- Assets/Scripts/ItemCtrl.cs
+++ Assets/Scripts/ItemCtrl.cs
@@ -10,16 +10,30 @@ public class ItemCtrl : MonoBehaviour {
 	//transform
 	Transform trans;
 
+	//wait for checkDestroy
+	WaitForSeconds waitForCheckY = new WaitForSeconds(0.1f);
+
 	void OnEnable() {
 		trans = transform;
 		Init();
+		StartCoroutine(RunCheckYForDestroy());
 	}
 
 	protected virtual void Init() { }
 
+	IEnumerator RunCheckYForDestroy() {
+		while(true) {
+			if(trans.position.y < -100) {
+				DestroyItem();
+			}
+			yield return waitForCheckY;
+		}
+	}
+
 	public void DestroyItem() {
 		trans.position = Vector3.one * 100f;
 		Init();
+		StopCoroutine(RunCheckYForDestroy());
 		ObjectPool.Release(gameObject);
 	}
 }
