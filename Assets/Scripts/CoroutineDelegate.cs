6c434574976292c00fb0952867af7ae0c3ab9616
YuGene <bbiggoagi@gmail.com>
2017-04-29 01:32:24 +0900

    코루틴딜리게이터, 오브젝트풀

---
+++ Assets/Scripts/CoroutineDelegate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CoroutineDelegate : MonoBehaviour {
+
+	//singleton
+	static CoroutineDelegate instance = null;
+	public static CoroutineDelegate Instance {
+		get {
+			if (!instance) {
+				GameObject gameObj = new GameObject("CoroutineDelegate");
+				instance = gameObj.AddComponent<CoroutineDelegate>();
+			}
+			return instance;
+		}
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(this);
+	}
+}
