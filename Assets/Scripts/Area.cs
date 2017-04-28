5bccdf094e6f27ef1de49ba881b1db84ecd18b71
YuGene <bbiggoagi@gmail.com>
2017-04-29 02:31:39 +0900

    이벤트, 애리어 스크립트 생성

---
+++ Assets/Scripts/Area.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area : MonoBehaviour {
+
+	//singleton
+	static Area[] instance = null;
+	public static Area[] Instance { get { return instance; } }
+
+	//index
+	[SerializeField]
+	int index;
+
+	//number of object in area
+	int objectCount;
+	int objectCount_ { get { return objectCount; } set { objectCount = value; } }
+	public int ObjectCount { get { return objectCount; } }
+
+	void Awake() {
+		InitializeSingleton();
+	}
+
+	void InitializeSingleton() {
+		if (instance == null) {
+			instance = new Area[2];
+		}
+		instance[index] = this;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if(collider.CompareTag("Item")) {
+			++objectCount_;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider) {
+		if (collider.CompareTag("Item")) {
+			--objectCount_;
+		}
+	}
+}
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+	//singleton
+	static EventManager instance = null;
+	public static EventManager Instance { get { return instance; } }
+
+	//initialize stage
+	InitializeStage initializeStage = new InitializeStage();
+
+	//Run stage
+	RunStage runStage = new RunStage();
+
+	//limit time
+	[SerializeField]
+	int limitTime = 60;
+
+	void Awake() {
+		instance = this;
+		Initialize();
+	}
+
+	void Initialize() {
+		Timer.LimitTime = limitTime;
+		initializeStage.Initialize();
+	}
+
+	void Start() {
+		RunStage();
+	}
+
+	void RunStage() {
+		runStage.Run();
+	}
+
+	void StopStage() {
+		runStage.Stop();
+	}
+}
+
+public class InitializeStage {
+
+	//fix resolution
+	InitializeResolution initializeResolution = new InitializeResolution();
+
+	public void Initialize() {
+		initializeResolution.FixResolution();
+	}
+}
+
+public class RunStage {
+
+	//timer for limit time
+	Timer timer = new Timer();
+
+	public void Run() {
+		timer.Run();
+	}
+
+	public void Stop() {
+		timer.Stop();
+	}
+}
+
+public class Timer {
+
+	//limit Time
+	static int limitTime;
+	static int limitTime_ { get { return limitTime; } set { limitTime = value; } }
+	public static int LimitTime { set { limitTime = value; } }
+
+	//wait a second
+	WaitForSeconds waitForASecond = new WaitForSeconds(1f);
+
+	//Result
+	Result result = new Result();
+
+	public void Run() {
+		CoroutineDelegate.Instance.StartCoroutine(RunASecond());
+	}
+
+	IEnumerator RunASecond() {
+		while(limitTime_ < 0) {
+			yield return waitForASecond;
+			--limitTime_;
+		}
+		Stop();
+		result.Run();
+	}
+
+	public void Stop() {
+		CoroutineDelegate.Instance.StopCoroutine(RunASecond());
+	}
+}
+
+public class Result {
+
+	public void Run() {
+
+	}
+}
