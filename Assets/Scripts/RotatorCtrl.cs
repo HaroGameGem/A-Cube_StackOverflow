304fba7f06413da078dce588628f64510a6899aa
HARO-NOTEBOOK <haro7488@gmail.com>
2017-04-29 16:54:55 +0900

    로테이터 상호작용시 색깔변화 ItemCtrl로 옮김

---
+++ Assets/Scripts/RotatorCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorCtrl : ItemCtrl {
+
+    bool isFlip = false;
+    float originRotSpeed = 0f;
+    public float rotSpeed = 90f;
+
+	// Use this for initialization
+	void Start () {
+        originRotSpeed = rotSpeed;
+	}
+
+    protected override void Init()
+    {
+        base.Init();
+        rotSpeed = originRotSpeed + Random.Range(0f, rotSpeed);
+        if (Random.Range(0, 2) == 0)
+        {
+            isFlip = true;
+            rotSpeed = Mathf.Abs(rotSpeed);
+        }
+        else
+        {
+            isFlip = false;
+            rotSpeed = -Mathf.Abs(rotSpeed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.Rotate(new Vector3(0f, 0f, 1f) * rotSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/WarmTailCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarmTailCtrl : MonoBehaviour {
+
+    WarmCtrl warmHead;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Item"))
+        {
+            ItemCtrl item = collision.gameObject.GetComponent<ItemCtrl>();
+            if(item.itemType == eItemType.Fire)
+            {
+                warmHead.Burn();
+            }
+        }
+        
+    }
+    
+}
